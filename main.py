9043244a377db066ed9d4f72c1a79d4c0fd0b323
Nadav Gover <nadavgover@gmail.com>
2020-04-18 08:09:40 +0300

    Minor changes in __main__

--- main.py
+++ main.py
@@ -108,22 +108,27 @@ if __name__ == '__main__':
     # test_spectrum_path = r"dataset/test_spectrum_apple_banana_original_size.npy"
     # train_labels_path = r"dataset/train_labels_apple_banana_original_size.npy"
     # test_labels_path = r"dataset/test_labels_apple_banana_original_size.npy"
-    fruits = ["apple", "banana", "mix"]
+    fruits = ["banana", "mix"]
     # train_spectrum_path = r"dataset/train_spectrum_after5_anal_data_original.npy"
     # test_spectrum_path = r"dataset/test_spectrum_after5_anal_data_original.npy"
     # train_labels_path = r"dataset/train_labels_after5_anal_data_original.npy"
     # test_labels_path = r"dataset/test_labels_after5_anal_data_original.npy"
+
     # train_spectrum_path = r"dataset/train_spectrum_after5_anal_10000.npy"
     # test_spectrum_path = r"dataset/test_spectrum_after5_anal_10000.npy"
     # train_labels_path = r"dataset/train_labels_after5_anal_10000.npy"
     # test_labels_path = r"dataset/test_labels_after5_anal_10000.npy"
-    train_spectrum_path = r"dataset/train_spectrum_after5_anal_5000.npy"
-    test_spectrum_path = r"dataset/test_spectrum_after5_anal_5000.npy"
-    train_labels_path = r"dataset/train_labels_after5_anal_5000.npy"
-    test_labels_path = r"dataset/test_labels_after5_anal_5000.npy"
-    main(create_dataset_now=False, num_epochs=15, kernel_size=(2, 2), padding=(1, 1), size_of_dataset=5000,
-         model_save_path=r"trained_models/model_kernel22_after5_anal_batch20_epochs15_data_5000.pth",
-         batch_size=20, train_now=True, predict_now=False, file_to_predict="banana neg.txt",
+
+    train_spectrum_path = r"dataset/train_spectrum_after5_anal_data_original_width900.npy"
+    test_spectrum_path = r"dataset/test_spectrum_after5_anal_data_original_width900.npy"
+    train_labels_path = r"dataset/train_labels_after5_anal_data_original_width900.npy"
+    test_labels_path = r"dataset/test_labels_after5_anal_data_original_width1900.npy"
+
+    main(create_dataset_now=True, num_epochs=50, kernel_size=(2, 2), padding=(1, 1), size_of_dataset=5000,
+         model_save_path=r"trained_models/model_kernel22_after5_anal_batch1_epochs30_data_original_width2100.pth",
+         batch_size=1, train_now=True, predict_now=False, file_to_predict="banana neg.txt",
          train_spectrum_path=train_spectrum_path, test_spectrum_path=test_spectrum_path,
-         train_labels_path=train_labels_path, test_labels_path=test_labels_path,
-         stretch_data=True, sample_location="anal", sample_time="after 5", fruits=fruits)
+         train_labels_path=train_labels_path, test_labels_path=test_labels_path, show_statistics=True,
+         stretch_data=False, sample_location="anal", sample_time="after 5", fruits=fruits, data_width=2100,
+         num_channels_layer1=30, num_channels_layer2=6, fc1_amount_output_nodes=500, fc2_amount_output_nodes=500,
+         fc3_amount_output_node=100)
